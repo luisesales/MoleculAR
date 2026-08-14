0aab2b062804568f3d5fb7717af539ba8d43b9cb
luisesales <duduosales@hotmail.com>
2026-08-14 18:48:55 -0300

    adding lat UI components on catalog scene

--- Assets/Assets/Scripts/SceneControllers/CatalogController.cs
+++ Assets/Assets/Scripts/SceneControllers/CatalogController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class CatalogController : MonoBehaviour
 {
+    public GameObject UI;
     private bool IsFiltersOpen = false;
     private bool IsFiltersActive = false;
 
-    public GameObject FiltersPanel;
+    private GameObject FiltersPanel;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        FiltersPanel = UI.transform.Find("FiltersPanel").gameObject;
     }
 
     // Update is called once per frame
